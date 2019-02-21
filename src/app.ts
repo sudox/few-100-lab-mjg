087f0df79e39ecc057d6af9510dddbcaa1c6cd68
Michael <mjg72@zips.uakron.edu>
2019-02-21 08:42:34 -0500

    More mocks for HTML and JS

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,17 @@
 export function runApp() {
     console.log('they see me running');
+    // Get list items
+    const billAmount = document.getElementById('billAmount');
+    const tipPercent = document.getElementById('tipPercent');
+    const tipAmount = document.getElementById('tipAmount');
+    const totalPaid = document.getElementById('totalPaid');
+    const tipDialog = document.getElementById('tipDialog');
+
+    tipDialog.innerText = "You are tipping 20%";
+
+    billAmount.innerHTML = "Test1";
+    tipPercent.innerHTML = "Test2";
+    tipAmount.innerHTML = "Test3";
+    totalPaid.innerHTML = "Test4";
+
 }
