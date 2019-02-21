d78d99b7de93345dfde246b2927e6766e16a93c6
Michael <mjg72@zips.uakron.edu>
2019-02-21 09:55:42 -0500

    Added function for bill input with tip calculation, need to sanitize input

--- src/app.ts
+++ src/app.ts
@@ -4,22 +4,30 @@ function tipClick(e: Event) {
     const buttonClicked = e.srcElement as HTMLInputElement;
 
     tipButtons.forEach((b: HTMLInputElement) => b.disabled = false);  // Enable all the tip buttons
-    buttonClicked.disabled = true; //Disable the source click button
+    buttonClicked.disabled = true; // Disable the source click button
 
-    const tipDialog = document.getElementById('tipDialog');
-    tipDialog.innerText = 'You are tipping ' + buttonClicked.innerText;
-
-    // Update list table
-    const bill = +document.getElementById('billAmount').innerText;
-    let tipAmount = bill * (+buttonClicked.innerText);
-    tipAmount = !!tipAmount ? tipAmount : 0.0;
-    document.getElementById('tipPercent').innerText = 'Tip Percentage: ' + buttonClicked.innerText;
-    document.getElementById('tipAmount').innerText = 'Amount of tip: $' + tipAmount;
+    document.getElementById('tipDialog').innerText = 'You are tipping ' + buttonClicked.innerText;
+    document.getElementById('tipPercent').innerText = "Tip Percentage: " + buttonClicked.innerText;
+    updateTotals();
 
 }
 
 function billChange() {
+    const billEntered = document.getElementById('totalBill') as HTMLInputElement;
+    document.getElementById('billAmount').innerText = "Bill Amount: $" + billEntered.value;
+    updateTotals();
+}
 
+function updateTotals() {
+    const billAmount = +document.getElementById('billAmount').innerText.replace(/^\D+/g, '');
+    let tipPercent = document.getElementById('tipPercent').innerText.replace(/^\D+/g, '');
+    tipPercent = tipPercent.substring(0, tipPercent.length - 1);
+    console.log(billAmount, tipPercent);
+    const tipAmount = +tipPercent * .01 * billAmount;
+    const totalPaid = billAmount + tipAmount;
+
+    document.getElementById('tipAmount').innerText = 'Amount of tip: $' + tipAmount;
+    document.getElementById('totalPaid').innerText = "Total to be Paid: $" + totalPaid;
 }
 
 export function runApp() {
