c74bd63ad86fbc37484e016e2f094d5673046b71
Michael <mjg72@zips.uakron.edu>
2019-02-21 09:14:37 -0500

    Added tip button javascript for clicks

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,32 @@
+
+function tipClick(e: Event) {
+    const tipButtons = document.getElementsByName('tipB');
+    const buttonClicked = e.srcElement as HTMLInputElement;
+
+    tipButtons.forEach((b: HTMLInputElement) => b.disabled = false);  // Enable all the tip buttons
+    buttonClicked.disabled = true; //Disable the source click button
+
+    const tipDialog = document.getElementById('tipDialog');
+    tipDialog.innerText = 'You are tipping ' + buttonClicked.innerText;
+}
+function test() { }
+
 export function runApp() {
     console.log('they see me running');
-    // Get list items
+
+    // List items
     const billAmount = document.getElementById('billAmount');
     const tipPercent = document.getElementById('tipPercent');
     const tipAmount = document.getElementById('tipAmount');
     const totalPaid = document.getElementById('totalPaid');
-    const tipDialog = document.getElementById('tipDialog');
 
-    tipDialog.innerText = "You are tipping 20%";
+    // Tip button hooks
+    document.getElementsByName('tipB').forEach(function (e) {
+        e.addEventListener('click', tipClick);
+    });
 
-    billAmount.innerHTML = "Test1";
-    tipPercent.innerHTML = "Test2";
-    tipAmount.innerHTML = "Test3";
-    totalPaid.innerHTML = "Test4";
-
+    billAmount.innerHTML = 'Test1';
+    tipPercent.innerHTML = 'Test2';
+    tipAmount.innerHTML = 'Test3';
+    totalPaid.innerHTML = 'Test4';
 }
